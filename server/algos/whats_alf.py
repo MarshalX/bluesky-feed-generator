6579d7da5be2e54474695c6cdcf21576e83d2400
Mwyann <github@perso.ylb.fr>
2023-09-11 23:20:55 +0200

    Minor fixes Some pyvenv additions to gitignore, better selection from the feed database, better handling of the end of the feed (cursor), and actually make the stream_stop_event work.

--- server/algos/whats_alf.py
+++ server/algos/whats_alf.py
@@ -8,20 +8,25 @@ uri = config.WHATS_ALF_URI
 
 
 def handler(cursor: Optional[str], limit: int) -> dict:
-    posts = Post.select().order_by(Post.indexed_at.desc()).order_by(Post.cid.desc()).limit(limit)
+    posts = Post.select().order_by(Post.cid.desc()).order_by(Post.indexed_at.desc()).limit(limit)
 
     if cursor:
+        if cursor == 'eof':
+            return {
+                'cursor': 'eof',
+                'feed': []
+            }
         cursor_parts = cursor.split('::')
         if len(cursor_parts) != 2:
             raise ValueError('Malformed cursor')
 
         indexed_at, cid = cursor_parts
         indexed_at = datetime.fromtimestamp(int(indexed_at) / 1000)
-        posts = posts.where(Post.indexed_at <= indexed_at).where(Post.cid < cid)
+        posts = posts.where(((Post.indexed_at == indexed_at) & (Post.cid < cid)) | (Post.indexed_at < indexed_at))
 
     feed = [{'post': post.uri} for post in posts]
 
-    cursor = None
+    cursor = 'eof'
     last_post = posts[-1] if posts else None
     if last_post:
         cursor = f'{int(last_post.indexed_at.timestamp() * 1000)}::{last_post.cid}'
