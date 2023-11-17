4f45776107e3ccaca1a631d8e4fe705aa83875a4
Umby <devnull@umby.dog>
2023-11-17 12:53:36 +1100

    Fixes related to bsky.social migration
    This points the firehose consumer to bsky.network, and checks for messages with empty blocks, preventing errors when one appears.

--- server/data_stream.py
+++ server/data_stream.py
@@ -79,7 +79,7 @@ def _run(name, operations_callback, stream_stop_event=None):
     if state:
         params = models.ComAtprotoSyncSubscribeRepos.Params(cursor=state.cursor)
 
-    client = FirehoseSubscribeReposClient(params)
+    client = FirehoseSubscribeReposClient(params,base_uri='wss://bsky.network/xrpc')
 
     if not state:
         SubscriptionState.create(service=name, cursor=0)
@@ -91,6 +91,8 @@ def _run(name, operations_callback, stream_stop_event=None):
             return
 
         commit = parse_subscribe_repos_message(message)
+        if not commit.blocks:
+            return
         if not isinstance(commit, models.ComAtprotoSyncSubscribeRepos.Commit):
             return
 
