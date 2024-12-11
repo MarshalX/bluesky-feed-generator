b37a66f4ceec6d6e0441cf0257a7da3ee545a4aa
Ilya Siamionau <ilya@marshal.dev>
2024-12-11 22:52:40 +0100

    Update atproto lib; fix unknown records; update cursor more rarely (#23)

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,9 @@
+import logging
+
 from app import app
+from server.logger import logger
 
 if __name__ == '__main__':
     # FOR DEBUG PURPOSE ONLY
+    logger.setLevel(logging.DEBUG)
     app.run(host='127.0.0.1', port=8000, debug=True)
