df602505f49b92a30a5205b58f96c9d7ba1f3481
samidh <samidh@users.noreply.github.com>
2025-01-25 13:48:54 -0800

    Fix `FEED_URI` empty check in `config.py` since in the `.env` sample it is set by default to the empty string (#26)
    Co-authored-by: Ilya (Marshal) <ilya@marshal.dev>

--- server/config.py
+++ server/config.py
@@ -14,15 +14,15 @@ FLASK_RUN_FROM_CLI = os.environ.get('FLASK_RUN_FROM_CLI')
 if FLASK_RUN_FROM_CLI:
     logger.setLevel(logging.DEBUG)
 
-if HOSTNAME is None:
+if not HOSTNAME:
     raise RuntimeError('You should set "HOSTNAME" environment variable first.')
 
-if SERVICE_DID is None:
+if not SERVICE_DID:
     SERVICE_DID = f'did:web:{HOSTNAME}'
 
 
 FEED_URI = os.environ.get('FEED_URI')
-if FEED_URI is None:
+if not FEED_URI:
     raise RuntimeError('Publish your feed first (run publish_feed.py) to obtain Feed URI. '
                        'Set this URI to "FEED_URI" environment variable.')
 
